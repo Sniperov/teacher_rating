57bcf7d9495bf53cb5971aeb07f72e1dc3b2e34c
nxmxrly <54706522+nxmxrly@users.noreply.github.com>
2021-03-14 16:41:23 +0300

    create user at admin

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\UserRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\User;
 
 class UserController extends Controller
 {
-    //
+    
+    public function reditUser(UserRequest $request , $id)
+    {
+
+        $data = $request->validated();
+
+        $user = User::findOrFail($id);
+
+        $user->update($user->all());
+
+        return response(['success' => true ] , 201 );
+    }
+
+    public function deleteUser($id)
+    {
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response(['success' => true ] , 201 );
+
+    }
+
+    public function getAllUsers()
+    {
+        return response(['success' => User::all()] , 201);
+    }
+
 }
